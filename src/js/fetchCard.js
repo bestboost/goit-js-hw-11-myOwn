3039df2f743e2b73978b71788a9c594f63c7280e
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2023-05-23 14:00:29 +0300

    responce function

--- src/js/fetchCard.js
+++ src/js/fetchCard.js
@@ -2,24 +2,17 @@ const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '29692752-5f9a27c26e6deec7970509d3f';
 const errorMessage = "Sorry, there are no images matching your search query. Please try again.";
 
-// const responseData = 
-//      [webformatURL,
-//      largeImageURL,
-//      tags, 
-//      likes,
-//      views,
-//      comments,
-//      downloads];
-
 fetch (`${BASE_URL}?key=${KEY}&q=cat&image_type=photo&orientation=horizontal&safesearch=true`)
 .then(r => r.json())
-.then(data => {
-         data.hits.map(hit => {
-          console.log("hit:", hit.webformatURL, 
-          "tags:", hit.tags)   
-     })
-})
+.then(data => fatchResponse(data))
 .catch(error => {
      console.log(error); 
      console.log(errorMessage);
 });
+
+function fatchResponse(data) {
+// const responseData = [webformatURL, largeImageURL, tags, likes, views, comments, downloads];
+          data.hits.map(hit => {
+           console.log("data:", hit)   
+      })
+ }
